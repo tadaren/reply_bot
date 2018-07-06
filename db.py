0a3a98f1b4646215b2be47a8177bf590f91355ee
Tadaren <tadaren@users.noreply.github.com>
2018-07-06 10:43:09 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,4 @@
-url = 'postgres://kofrxrfklpeeku:38d19e37dbeaf312961fe84b0fe3dcb5ac9adc1c8b1342b2f6c7725d3e194d3d@ec2-54-204-44-140.compute-1.amazonaws.com:5432/ddcebjdne2t85m'
-secret = '6d78418a981c1dadaa1798156123f488'
-token = 'PxpPl5fThdhV9SzTpYkS4whB+ADwsD4HcdwD58PyjQ4cR/JjtfGlawEM2tj6fK4yWGm/AqeeKrxmcLiKxWxU3mAXPIfcR7EVBmux1Bh1FIFLLbY6q/xIyKkCdBgKYQBdUMhqZKme/ZUPXodRx5WR2AdB04t89/1O/w1cDnyilFU='
-
-config = {
-    'NAME': 'ddcebjdne2t85m', 
-    'USER': 'kofrxrfklpeeku', 
-    'PASSWORD': '38d19e37dbeaf312961fe84b0fe3dcb5ac9adc1c8b1342b2f6c7725d3e194d3d', 
-    'HOST': 'ec2-54-204-44-140.compute-1.amazonaws.com', 
-    'PORT': 5432, 
-    'CONN_MAX_AGE': 0, 
-    'ENGINE': 'django.db.backends.postgresql_psycopg2'
-    }
+config = {}
 
 import psycopg2
 

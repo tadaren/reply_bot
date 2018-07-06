4e9cbee9cc4fb5508c0d17ce56568d0f2ecd8d98
tadaren <ptymaato@yahoo.co.jp>
2018-07-06 10:34:49 +0900

    fixed

--- db.py
+++ db.py
@@ -28,7 +28,7 @@ def insert(key, value):
     connection = psycopg2.connect(host=config['HOST'], port=config['PORT'], database=config['NAME'], user=config['USER'], password=config['PASSWORD'])
     cur = connection.cursor()
     try:
-        cur.execute("insert into reply_map values ?, ? ", (key, value))
+        cur.execute("insert into reply_map values (%s, %s)", (key, value))
         connection.commit()
     except:
         pass
